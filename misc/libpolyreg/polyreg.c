888f1256f98cb1ab9be5a5b3534a255d3217cc15
Alin Jerpelea <alin.jerpelea@sony.com>
2018-08-07 13:05:32 +0200

    libpolyreg: Fix libcutils private headers
    Use cutils/android_filesystem_config.h and log/log.h instead.
    
    Test: m -j
    Test: BOARD_VNDK_VERSION=current m -j
    Test: mma -j
    Test: BOARD_VNDK_VERSION=current mma -j
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>
    Change-Id: I7e1ad4bd49ce2895575f02a2b6394933583ce9dd

--- misc/libpolyreg/polyreg.c
+++ misc/libpolyreg/polyreg.c
@@ -28,8 +28,8 @@
 
 #include <libpolyreg/polyreg.h>
 
-#include <private/android_filesystem_config.h>
-#include <utils/Log.h>
+#include <cutils/android_filesystem_config.h>
+#include <log/log.h>
 
 #define LOG_TAG		"PolyReg"
 
