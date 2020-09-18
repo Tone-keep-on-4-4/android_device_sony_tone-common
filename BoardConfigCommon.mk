cf5d0e2a8b142ce54fa09738646ad11626b4c7ba
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    tone-common: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,7 +182,7 @@ BOARD_ROOT_EXTRA_FOLDERS := odm
 VENDOR_SECURITY_PATCH := 2019-05-01
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
