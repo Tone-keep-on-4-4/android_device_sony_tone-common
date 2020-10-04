8835db95bc8e49885dd8697d0d7bb723ae7ec17a
Erik Castricum <git@cas-online.nl>
2020-10-04 10:49:06 +0200

    tone: build camera from source
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- tone.mk
+++ tone.mk
@@ -134,7 +134,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    camera.msm8996
 
 PRODUCT_PACKAGES += \
     libmmcamera_interface \
