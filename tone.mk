304880eed5ef67b458c20d2a8ce93a64f2504173
sjllls <2012ji123@gmail.com>
2021-01-08 12:54:28 +0800

    tone: Build vendor.qti.hardware.camera.device for IMS

--- tone.mk
+++ tone.mk
@@ -225,7 +225,9 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    ims-ext-common
+    ims-ext-common \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Input
 PRODUCT_COPY_FILES += \
