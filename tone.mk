728f9cf346f5d74ab384648448d9d7f9b86ffbea
Erik Castricum <git@cas-online.nl>
2021-01-11 07:18:14 +0100

    tpne: adjust audio config
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I5633bc7b0a42e5fba13e383ac693f265cc2df9cb

--- tone.mk
+++ tone.mk
@@ -93,12 +93,14 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0-impl:32 \
-    android.hardware.audio.effect@5.0-impl:32 \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
     audio.usb.default \
+    libaacwrapper \
     libaudio-resampler \
     libaudioroute \
     libqcompostprocbundle \
