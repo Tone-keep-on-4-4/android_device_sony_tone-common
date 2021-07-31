0c667a687a40c61adf95975dba07c0ac6d8852ec
Erik Castricum <git@cas-online.nl>
2021-07-31 18:50:36 +0200

    tone: add sony thermanager and compile it
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: Iaa917cd3c5484317dffa0f73c978779d2693eafc

--- tone.mk
+++ tone.mk
@@ -426,6 +426,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Thermanager
+PRODUCT_PACKAGES += \
+    thermanager
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
