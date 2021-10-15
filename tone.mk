c9f139b15cbefaaf4f127fadc30ce19dc6aa5036
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-15 11:48:48 +0800

    tone-common: Build all extra VNDK versions
    * Basically make system a GSI, but this should avoid us the headache made by VNDK libs not being built based on proprietary interfaces
    
    Change-Id: I1ca876b5d2a82d41af76f4c327b89a181bb3d6bd
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- tone.mk
+++ tone.mk
@@ -457,6 +457,8 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
 # VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
+
 $(foreach target, $(shell cat $(LOCAL_PATH)/vndk.txt), $(eval PRODUCT_PACKAGES += $(target).vendor))
 
 PRODUCT_PACKAGES += \
