2aa0f748f55c389394ba569e05b80aaa737ebd47
sjllls <sjllls@live.com>
2022-08-12 21:28:02 +0800

    tone: Add missing libraries

--- tone.mk
+++ tone.mk
@@ -407,6 +407,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
